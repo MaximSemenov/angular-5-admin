abe4e89fd74e1cacd83a1ac0f802e715e435244a
MaximSemenov <maxim.n.semenov@gmail.com>
2018-01-23 15:09:07 -0500

    Logic is added for the PaginationComponent to determine how many pages to display in the its view

--- src/app/users/pagination/pagination.component.ts
+++ src/app/users/pagination/pagination.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SimpleChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnChanges {
+
   @Input() quantityOfUsers;
+  public pages: number[] = [1, 2, 3, 4, 5];
 
+  constructor() { }
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    // this.activatedRoute.queryParams.subscribe(params => console.log(params));
+  ngOnInit() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if (!changes['quantityOfUsers'].isFirstChange()) {
+      const lastPage = Math.ceil(this.quantityOfUsers / 10);
+      this.pages = Array.from(Array(lastPage + 1).keys()).slice(1);
+    }
+
   }
-
-  ngOnInit() {
-
-  }
-
 }
