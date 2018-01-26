b5891317f909383fa67fc30ccffda79f6356b0df
MaximSemenov <maxim.n.semenov@gmail.com>
2018-01-26 00:19:06 -0500

    Refactored the User List Component

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -8,7 +8,7 @@ import { SimpleChanges } from '@angular/core/src/metadata/lifecycle_hooks';
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css']
 })
-export class UsersListComponent implements OnInit {
+export class UsersListComponent implements OnInit, OnChanges {
 
   public activePage: number;
   public userFilter: number;
@@ -20,17 +20,21 @@ export class UsersListComponent implements OnInit {
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe((params) => {
-      // debugger;
-      if (!params || !params.page) {
+
+      if (!params || !params.page || !this.users) {
         this.router.navigateByUrl('/users?page=1');
         return false;
       }
-      const page = +params.page;
-
-      this.userFilter = page * 10;
-      this.filteredUsers = this.users.filter((item, index) => index < this.userFilter && index >= this.userFilter - 10);
-
+      this.userFilter = +params.page * 10;
+      console.log(this.userFilter);
+      this.filteredUsers = this.users.slice(this.userFilter - 10, this.userFilter);
     });
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if (!changes['users'].isFirstChange()) {
+      this.filteredUsers = this.users.slice(0, 10);
+    }
+  }
 }
